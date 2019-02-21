351c8b0198e29fdba5fd1632e4a8a14160793f69
Boo <almeidaribeiro.b@gmail.com>
2019-02-21 00:15:45 -0300

    falta arrumar texto result

---
+++ index.js
@@ -0,0 +1,111 @@
+function setBtnState() {
+	const text = document.getElementById("messageArea").value;
+	if (text === ""){
+		document.getElementById("button").disabled = true; 
+	} else {
+		document.getElementById("button").disabled = false;
+	}
+
+	
+}
+
+messageArea.addEventListener("keyup", textSize);
+
+function textSize(event){
+
+	
+
+	messageArea.style.cssText = "height:auto; padding:0";
+	messageArea.style.cssText = "height:" + messageArea.scrollHeight + "px";
+
+}
+
+//function updateCounter(){
+
+	//const text = document.getElementById("messageArea").value;
+
+	//let counter = 140;
+
+	//counter = 140 - text.length;
+
+	//if (counter > 140){
+	//	document.getElementById("button").disabled = true;
+	//} 
+	//if (counter > 120){
+	//	document.getElementById("counter").style.color = "yellow";
+	//} 
+	//if (counter > 130) {
+	//	document.getElementById("counter").style.color = "red";
+	//}
+
+	//document.getElementById("counter").innerHTML = counter;
+
+//}
+
+
+function updateCounter(){
+
+	const text = document.getElementById("messageArea").value;
+
+	let counter = 140;
+
+	counter = 140 - text.length;
+
+	if (text.length > 140){
+		document.getElementById("button").disabled = true;
+	} 
+	if (text.length > 120 && text.length <= 130){
+		document.getElementById("counter").style.color = "yellow";
+	} 
+	else if (text.length > 130) {
+	document.getElementById("counter").style.color = "red";
+	} else{
+		document.getElementById("counter").style.color = "black";
+	}
+
+	document.getElementById("counter").innerHTML = counter;
+
+}
+
+document.getElementById("messageArea").addEventListener("keyup", function(){
+
+	setBtnState();
+	
+	updateCounter();
+
+	textAreaResize()
+	
+
+	
+	
+	
+});
+
+
+document.getElementById("button").addEventListener("click", function () {
+
+	let message = document.getElementById("messageArea");
+	let messageOutput = document.getElementById("result");
+	let day = new Date();
+
+	let dayString = String(day);
+
+	let newTweet = document.createElement("p");
+	let newDay = document.createElement("p");
+	
+	
+
+	newTweet.innerHTML = message.value;
+	newDay.innerHTML = dayString.slice(0,21);
+
+	
+	newTweet.appendChild(newDay);
+	messageOutput.appendChild(newTweet);
+
+
+	document.getElementById("button").disabled = true;
+	document.getElementById("counter").innerHTML = 140;
+	message.value = "";
+});
+
+
