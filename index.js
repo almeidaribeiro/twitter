04602c6d0167fad7231bf1bac6f5b95d37778173
almeidaribeiro <almeidaribeiro.b@gmail.com>
2019-07-09 19:42:49 -0300

    reposicinamento de elementos

--- index.js
+++ index.js
@@ -16,11 +16,8 @@ function textSize(event){
 
 function updateCounter(){
 	const text = document.getElementById("messageArea").value;
-
 	let counter = 140;
-
 	counter = 140 - text.length;
-
 	if (text.length > 140){
 		document.getElementById("button").disabled = true;
 	} 
@@ -32,37 +29,27 @@ function updateCounter(){
 	} else{
 		document.getElementById("counter").style.color = "black";
 	}
-
 	document.getElementById("counter").innerHTML = counter;
 }
 
 document.getElementById("messageArea").addEventListener("keyup", function(){
 	setBtnState();
-	
 	updateCounter();
-
-	textAreaResize()
+	//textAreaResize()
 });
 
 
 document.getElementById("button").addEventListener("click", function () {
 	let message = document.getElementById("messageArea");
 	let messageOutput = document.getElementById("result");
-	
 	let day = new Date();
 	let dayString = String(day);
-
 	let newTweet = document.createElement("p");
 	let newDay = document.createElement("div");
-	
 	newTweet.innerHTML = message.value;
 	newDay.innerHTML = dayString.slice(0,21);
-
-	
 	newTweet.appendChild(newDay);
 	messageOutput.prepend(newTweet);
-
-
 	document.getElementById("button").disabled = true;
 	document.getElementById("counter").innerHTML = 140;
 	message.value = "";
